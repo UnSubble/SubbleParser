f90b6d2d89c5b7ca9b8dbade065af32b17f65288
beto0000 <132066307+beto0000@users.noreply.github.com>
2024-04-15 18:06:16 +0300

    .

---
+++ src/main/java/com/unsubble/yaml/YAMLParser.java
@@ -0,0 +1,9 @@
+package com.unsubble.yaml;
+
+import com.unsubble.parser.Parser;
+
+
+public interface YAMLParser extends Parser {
+
+
+}
---
+++ src/main/java/com/unsubble/yaml/dom/Key.java
@@ -0,0 +1,16 @@
+package com.unsubble.yaml.dom;
+
+import java.util.List;
+
+public interface Key {
+    String getName();
+    int getAge();
+    String getOccupaiton();
+    List<String> getHobbies();
+
+
+
+
+
+
+}
