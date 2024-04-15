b2a1f223f207de4eb671ea8751dd2d2c4706da4d
beto0000 <132066307+beto0000@users.noreply.github.com>
2024-04-15 22:20:40 +0300

    .

---
+++ src/main/java/com/unsubble/main/BetoDemo.java
@@ -0,0 +1,27 @@
+package com.unsubble.main;
+
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+
+public class BetoDemo {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args){
+
+        
+        try{
+
+
+
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
--- src/main/java/com/unsubble/yaml/BaseYAMLParser.java
+++ src/main/java/com/unsubble/yaml/BaseYAMLParser.java
@@ -1,21 +1,38 @@
 package com.unsubble.yaml;
 
 
+import com.unsubble.validators.Validator;
+import com.unsubble.validators.xml.XMLDeclarationParser;
+import com.unsubble.validators.xml.XMLValidator;
+import org.apache.logging.log4j.LogManager;
 import org.yaml.snakeyaml.Yaml;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileNotFoundException;
 
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.*;
 
 public class BaseYAMLParser implements YAMLParser{
 
+    private Path path;
+    private Validator validator;
+    public BaseYAMLParser(Path path) throws FileNotFoundException {
+        this.path=path;
+        validator=new XMLValidator();
+    }
 
+    public void parseYaml() throws FileNotFoundException {
 
-
-
-    public BaseYAMLParser() throws FileNotFoundException {
-
+        try (Reader reader = new FileReader(path.toFile(), StandardCharsets.UTF_8)) {
+            String content = XMLDeclarationParser.parseDeclaration(reader);
+            LogManager.getLogger().info(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String toString(){
